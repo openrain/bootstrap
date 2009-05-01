b011cbccabb369f0ded70b8ad976f8db1313d8f4
Marc Chung <mchung@gmail.com>
2009-04-30 18:34:52 -0700

    Adding image science

---
+++ slicehost/56-image-science.sh
@@ -0,0 +1,14 @@
+#
+# Ubuntu 8.0.4
+#
+# Installs FreeImage. Installs image_science gem if Rubygems is installed
+#
+
+apt-get update -y
+
+apt-get install -y libfreeimage-dev libfreeimage3 
+
+if [ -e "`which gem`" ]
+then
+  gem install image_science
+fi
